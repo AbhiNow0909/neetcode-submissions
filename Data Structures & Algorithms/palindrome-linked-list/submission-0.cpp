d27065654317369aacaeade4236da00bef8d7ab1
Abhinav B <152417072+AbhiNow0909@users.noreply.github.com>
2026-08-07 22:52:20 +0530

    Add: palindrome-linked-list - submission-0

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-0.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode * slow =head, *fast = head;
+
+        while(fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow= slow->next;
+        }
+
+        ListNode* prev = nullptr;
+
+        while(slow)
+        {
+            ListNode* temp = slow->next;
+            slow->next = prev;
+            prev=slow;
+            slow = temp;
+        }
+
+        ListNode* left = head, *right = prev;
+        while(right && left)
+        {
+            if(left->val != right->val)
+            {
+                return false;
+            }
+            left = left->next;
+            right = right->next;
+        }
+
+        return true;
+    }
+};
