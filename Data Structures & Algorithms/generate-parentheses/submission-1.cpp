cce88b855792e549f0723419780fe464b18c3e49
Abhinav B <152417072+AbhiNow0909@users.noreply.github.com>
2026-08-16 00:22:17 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    void backtrack(int openN, int closeN, int n, vector<string> &res, string &stack)
+    {
+        if(openN == closeN && openN == n)
+        {
+            res.push_back(stack);
+            return;
+        }
+
+        if(openN<n)
+        {
+            stack += '(';
+            backtrack(openN+1,closeN,n,res,stack);
+            stack.pop_back();
+        }
+
+        if(openN>closeN)
+        {
+            stack += ')';
+            backtrack(openN,closeN+1,n,res,stack);
+            stack.pop_back();
+        }
+
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string stack;
+        backtrack(0,0,n,res,stack);
+        return res;
+    }
+};
